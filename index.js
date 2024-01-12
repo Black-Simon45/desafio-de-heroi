a318a0d36e5c5f187aabd7501373446fd7febce5
Black-Simon45 <cleiton.simon33@gmail.com>
2024-01-11 22:45:43 -0300

    meu quarto commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let = heroi
+let = heroi;
 console.log("Digite o nome do herói: ");
 
-let = xp
+let = xp;
 console.log("Digite a quantidade de experiências: ");
 
 let nivel;
@@ -9,19 +9,19 @@ let nivel;
 if (xp < 1000){
     nivel = "Ferro";
 } else if (xp >= 1001 && xp <= 2000){
-    nivel = "Bronze"
+    nivel = "Bronze";
 } else if (xp >= 2001 && xp <= 5000){
-    nivel = "Prata"
+    nivel = "Prata";
 } else if (xp >= 5001 && xp <= 7000){
-    nivel = "Ouro"
+    nivel = "Ouro";
 } else if (xp >= 7001 && xp <= 8000){
-    nivel = "Platina"
+    nivel = "Platina";
 } else if (xp >= 8001 && xp <= 9000){
-    nivel = "Ascendente"
+    nivel = "Ascendente";
 } else if (xp >= 9001 && xp <= 10000){
-    nivel = "Imortal"
+    nivel = "Imortal";
 } else if (xp >= 10001){
-    nivel = Radiante
+    nivel = "Radiante";
 }
 for (let i = 0; i < 3; i++);    
 console.log(heroi + "atingiu o nível " + nivel + "!");

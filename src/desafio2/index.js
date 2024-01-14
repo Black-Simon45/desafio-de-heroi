1d84ade7e7b3589462dad104d579b080a4db3131
Black-Simon45 <cleiton.simon33@gmail.com>
2024-01-14 01:50:42 -0300

    meu quinto commit

---
+++ src/desafio2/index.js
@@ -0,0 +1,22 @@
+function calcularNivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+
+if (vitoria < 10){
+    nivel = "Ferro";
+} else if (vitoria >= 11 && vitoria <= 20{
+    nivel = "Bronze";
+} else if (vitoria >= 21 && vitoria <= 50{
+    nivel = "Prata";
+} else if (vitoria >= 51 && vitoria <= 80{
+    nivel = "Ouro";
+} else if (vitoria >= 81 && vitoria <= 90{
+    nivel = "Diamante";
+} else if (vitoria >= 91 && vitoria <=100{
+    nivel = "Lendário";
+} else {
+    nivel = "Imortal";
+}
+return { saldoVitorias, nivel };
+}
